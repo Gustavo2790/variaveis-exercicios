3b81a59121c8ecf2cc09c5bf99442194fcd0014a
Gustavo <gustavosoares2790@gmail.com>
2023-08-22 18:25:12 -0300

    Alterações Exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+/*
+const respostaA = prompt('Você se alimentou hoje?')
+const respostaB = prompt('Você é fumante?')
+const respostaC = prompt('Você tem animais de estimação?')
+
+console.log(respostaA)
+console.log(respostaB)
+console.log(respostaC)*/
+
+const perguntaA = "Vovê se alimentou hoje?"
+const perguntaB = "Você é fumante?"
+const perguntaC = "Você tem animais de estimação?"
+
+const respostaA = prompt(perguntaA)
+const respostaB = prompt(perguntaB)
+const respostaC = prompt(perguntaC)
+
+console.log(perguntaA , respostaA)
+console.log(perguntaB , respostaB)
+console.log(perguntaC , respostaC)
