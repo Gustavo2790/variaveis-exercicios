b889cbe7650cd15787b80ee53d25478ebb494fe3
Gustavo <gustavosoares2790@gmail.com>
2023-08-22 19:48:13 -0300

    Alterações Exercício 3

---
+++ exercicio3/script.js
@@ -0,0 +1,57 @@
+
+let nome = "Nome completo:"
+let dataNascimento = "Data de nascimento(utilize as /):"
+let endereco = "Endereço:"
+let cpf = "CPF:"
+let escolaridade = "Escolaridade:"
+let cnh = "Possui CNH?"
+let filhos = "Quantos filhes possui?"
+let cargo = "Cargo atual:"
+let salario = "Salário:"
+let comissao = "Recebe comissão? Se sim, aperte em confirme, caso não, clique em cancel"
+let admissao = "Informe o ano de admissão:"
+
+let respostaNome = prompt(nome)
+let respostaDataNascimento = prompt(dataNascimento)
+let respostaEndereco = prompt(endereco)
+let respostaCpf = prompt(cpf)
+let respostaEscolaridade = prompt(escolaridade)
+let respostaCnh = prompt(cnh)
+let respostaFilhos = Number(prompt(filhos))
+let respostaCargo = prompt(cargo)
+let respostaSalario = prompt(salario)
+let respostaComissao = confirm(comissao)
+let respostaAdmissao = Number(prompt(admissao))
+
+console.log(nome , respostaNome)
+console.log(typeof nome , typeof respostaNome)
+
+console.log(dataNascimento , respostaDataNascimento)
+console.log(typeof dataNascimento , typeof respostaDataNascimento)
+
+console.log(endereco , respostaEndereco)
+console.log(typeof endereco , typeof respostaEndereco)
+
+console.log(cpf , respostaCpf)
+console.log(typeof cpf , typeof respostaCpf)
+
+console.log(escolaridade , respostaEscolaridade)
+console.log(typeof escolaridade , typeof respostaEscolaridade)
+
+console.log(cnh , respostaCnh)
+console.log(typeof chh , typeof respostaCnh)
+
+console.log(filhos , respostaFilhos)
+console.log(typeof filhos , typeof respostaFilhos)
+
+console.log(cargo , respostaCargo)
+console.log(typeof cargo , typeof respostaCargo)
+
+console.log(salario , respostaSalario)
+console.log(typeof salario , typeof respostaSalario)
+
+console.log(comissao , respostaComissao)
+console.log(typeof comissao , typeof respostaComissao)
+
+console.log(admissao , respostaAdmissao)
+console.log(typeof admissao , typeof respostaAdmissao)
